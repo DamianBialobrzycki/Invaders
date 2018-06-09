3041775983e402130f38d2088a82959dee7fe573
Damian Białobrzycki <damianbialobrzycki@gmail.com>
2018-06-09 18:28:45 +0200

    Added sprites, enumerators and some Model type classes.

---
+++ Invaders/Model/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invaders.Model
+{
+    class Player : Ship
+    {
+        static PlayerSize Size = new PlayerSize(25, 15);
+        const double Speed = 10;
+
+        public Player(Point location, Size size) : base(location, size)
+        {
+            
+        }
+    }
+}
